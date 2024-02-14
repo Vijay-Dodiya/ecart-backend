32a57df0ccbd18f83f161a5f08c95d43046846a2
Vijay Dodiya <dodiyavijay047@gmail.com>
2024-02-14 21:42:07 +0530

    init project

---
+++ schema/resolvers.js
@@ -0,0 +1,9 @@
+import Item from './item/item.resolvers.js'
+import RootQuery from './root-query/root-query.resolvers.js'
+import RootMutation from './root-mutation/root-mutation.resolvers.js'
+
+export default {
+  Item,
+  RootQuery,
+  RootMutation
+}
